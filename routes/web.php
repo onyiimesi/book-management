160ef5285145a04d5e91340db0b36315d791c971
onyiimesi <onyedikachukwu62@gmail.com>
2024-07-25 21:00:21 +0100

    Added home route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [BookController::class, 'index']);
 Route::resource('books', BookController::class);
